dfee76e5e521026f3dd1368c423d1ac7c28474e2
Chandra <shekar.ym@gmail.com>
2024-01-07 04:55:37 +0000

    chapter 8 Functions

---
+++ chapter08/8-1.py
@@ -0,0 +1,4 @@
+def display_message():
+    print("I am learnig about functions in this chapter")
+
+display_message()
---
+++ chapter08/greeter.py
@@ -0,0 +1,11 @@
+def greet_me():
+    """print a simple message"""
+    print("Hello Chandra !")
+
+greet_me()
+
+def greet_user(ugit ssername):
+    """greet a user with username"""
+    print(f"Hello, {username}")
+
+greet_user('Walter White')
