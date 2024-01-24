3bc4709a1bd529cb7c8c34d89d535c9d06c97d53
Chandra <shekar.ym@gmail.com>
2024-01-24 09:31:21 +0000

    Files commit 2

--- chapter10/file_reader.py
+++ chapter10/file_reader.py
@@ -2,12 +2,30 @@ from pathlib import Path
 
 # path = Path('pi_digits.txt')
 
-path = Path('/workspaces/python-crash-course/chapter10/pi_digits.txt')
+# path = Path('/workspaces/python-crash-course/chapter10/pi_digits.txt')
+# contents = path.read_text()
+# # print(contents)
+
+
+# lines = contents.splitlines()
+# pi_string = ''
+# # for line in lines:
+# #     print(line)
+
+# for line in lines:
+#     pi_string += line.lstrip()
+
+# print(pi_string)
+# print(len(pi_string))
+
+path = Path('/workspaces/python-crash-course/chapter10/pi_million_digits.txt')
 contents = path.read_text()
-
 lines = contents.splitlines()
+pi_string = ''
 for line in lines:
-    print(line)
+    pi_string += line.lstrip()
+
+print(f"{pi_string[:52]}.....")
+print(len(pi_string))
 
 
-# print(contents)
