d00d53f0c1484f6bc1aadc69835f1173bd73d410
Chandra <shekar.ym@gmail.com>
2024-01-30 11:23:47 +0000

    30012024-1

---
+++ chapter10/10-2.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+path = Path('/workspaces/python-crash-course/chapter10/learning_python.txt')
+contents = path.read_text()
+lines = contents.splitlines()
+
+about_python = ''
+
+for line in lines:
+    line.replace('Python','C')
+    about_python += line
+
+print(about_python)
