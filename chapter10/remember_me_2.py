03173221cc18979fcbbf36ea04280094e900626f
Chandra <shekar.ym@gmail.com>
2024-02-04 04:03:37 +0000

    storing data 3

---
+++ chapter10/remember_me_2.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import json
+
+path = Path('username.json')
+if path.exists():
+    contents = path.read_text()
+    username = json.loads(contents)
+    print(f"Welcome back, {username}")
+else:
+    username = input("Enter your username: ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+    print(f"We will remember you when you are back, {username}")
