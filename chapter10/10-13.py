2ca7000777e22396ba57f2892e5d3606d6cd5807
Chandra <shekar.ym@gmail.com>
2024-02-08 11:44:28 +0000

    storing data 6

--- chapter10/10-13.py
+++ chapter10/10-13.py
@@ -1,13 +1,22 @@
 from pathlib import Path
 import json
 
-user_name = input("Enter your user name: ")
-age = input("Enter your age: ")
-location = input("Enter your city: ")
+# user_name = input("Enter your user name: ")
+# age = input("Enter your age: ")
+# location = input("Enter your city: ")
 
-user_info = {'user_name' : user_name,
-             'age': age,
-             'location': location
-             }
+# user_info = {'user_name' : user_name,
+#              'age': age,
+#              'location': location
+#              }
 
-print(user_info)
+path = Path('user_info.json')
+# contents = json.dumps(user_info)
+# path.write_text(contents)
+
+contents = path.read_text()
+user_info = json.loads(contents)
+print("Welcome back.")
+print(f"Your user name is {user_info['user_name']}.")
+print(f"Your age is {user_info['age']} years.")
+print(f"Your location is {user_info['location']}.")
