4558da1d90594c42c627704aa13985eada367e96
Chandra <shekar.ym@gmail.com>
2024-02-08 11:46:30 +0000

    storing data 10-14

---
+++ chapter10/10-14.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import json
+
+def get_stored_username(path):
+    """Get stored user name if available"""
+    if path.exists():
+        contents = path.read_text()
+        user_name = json.loads(contents)
+        return user_name
+    else:
+        return None
+
+def get_new_username(path):
+    "Prompt for a new user name"
+    user_name = input("What is your user name? ")
+    contents = json.dumps(user_name)
+    path.write_text(contents)
+    return user_name
+
+def greet_user():
+    """Greet user by name"""
+    path = Path('username5.json')
+    user_name = get_stored_username(path)
+    if user_name:
+        print(f"Welcome back, {user_name}")
+    else:
+        user_name = get_new_username(path)
+        print(f"We will remember you when you are back, {user_name}")
+
+greet_user()
+
