53a0cc119be27c8ff013d7b3bd327dad2d5ecc77
Chandra <shekar.ym@gmail.com>
2024-02-27 11:35:58 +0000

    Chapter 11 testing failure scenario

--- chapter11/name_function.py
+++ chapter11/name_function.py
@@ -1,4 +1,4 @@
-def get_formatted_name(first, last):
+def get_formatted_name(first,middle,last):
     """Function to return a formatted name based on first and last names"""
-    full_name = f"{first} {last}"
+    full_name = f"{first} {middle} {last}"
     return full_name.title()
