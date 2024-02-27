9cb4b6983c6c25daa12d141a93bf13b74993fea5
Chandra <shekar.ym@gmail.com>
2024-02-27 11:27:23 +0000

    testing

--- chapter11/names.py
+++ chapter11/names.py
@@ -1,2 +1,13 @@
 from name_function import get_formatted_name
 
+print("Enter 'q' anytime to exit")
+while True:
+    first = input("Enter your first name: ")
+    if first == 'q':
+        break
+    last  = input("Enter your last name: ")
+    if last == 'q':
+        break
+
+    formatted_name = get_formatted_name(first, last)
+    print(f"Neatly formatted name : {formatted_name}")
