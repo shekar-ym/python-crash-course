b56377e953d42a7357195739ba83b74dc14a2a46
Chandra <shekar.ym@gmail.com>
2024-03-07 11:16:39 +0000

    Running the Game in Fullscreen Mode

--- chapter12/alien_invasion/alien_invasion.py
+++ chapter12/alien_invasion/alien_invasion.py
@@ -11,7 +11,11 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get.rect().width
+        self.settings.screen_height = self.screen.get.rect().height
+
+        # self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
